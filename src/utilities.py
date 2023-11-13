3c1b2b14b4a7453c9f9e2eebdd9c0170ad41f57e
alanyeung95 <45751387+alanyeung95@users.noreply.github.com>
2023-11-12 19:27:23 -0500

    refactor: Add updated openai api usage

--- src/utilities.py
+++ src/utilities.py
@@ -2,13 +2,14 @@ import http.client
 import json
 import os
 
+from openai import OpenAI
 from dotenv import load_dotenv
 
 load_dotenv()
 
 OPENAI_API_KEY = os.getenv('OPENAI_API_KEY') 
 OPENAI_API_ENDPOINT = os.getenv('OPENAI_API_ENDPOINT') 
-print(OPENAI_API_ENDPOINT)
+
 def call_openai_api(role, prompt):
     conn = http.client.HTTPSConnection(OPENAI_API_ENDPOINT)
 
@@ -29,3 +30,18 @@ def call_openai_api(role, prompt):
     conn.close()
 
     return data.decode("utf-8")
+
+def call_openai_api_v2(system_context, user_prompt, top_p, temperature=1):
+    client = OpenAI()
+
+    response = client.chat.completions.create(
+        model="gpt-3.5-turbo-16k",
+        messages=[
+            {"role": "system", "content": system_context},
+            {"role": "user", "content": user_prompt}
+        ],
+        top_p=top_p,
+        temperature=temperature
+    )
+    #return response.choices[0].message.content
+    return response
